d57ef65b247a7497b1f25fe6e84f159aa51e153c
Higor Denomar <higordenomar@gmail.com>
2023-11-05 20:58:59 -0300

    refactor: messages of CLI

--- src/cmd.ts
+++ src/cmd.ts
@@ -5,15 +5,13 @@ import { generateFiles } from './service'
 async function main() {
   console.log('\n** Only .txt files supported **\n')
 
-  const filePath = readlineSync.question(
-    'Type the file path without extension\n(default: input): ',
-    {
-      isFile: true,
-      defaultInput: 'input',
-    }
-  )
+  const filePath = readlineSync.question('Type the file path: ', {
+    exists: true,
+    limit: /\.txt$/i,
+    limitMessage: '\nError, please enter the path of a .txt file\n',
+  })
 
-  generateFiles(filePath + '.txt')
+  generateFiles(filePath)
 }
 
 main()
