dbea31351aa93d5c63ee7a8dd906290db8a49f1d
Higor Denomar <higordenomar@gmail.com>
2023-11-05 12:50:55 -0300

    feat: create CLI

--- src/cmd.ts
+++ src/cmd.ts
@@ -1,1 +1,21 @@
-console.log('Hello world')
+import readlineSync from 'readline-sync'
+
+async function main() {
+  console.log('\n** Only .txt files supported **\n')
+
+  const filePath = readlineSync.question(
+    'Type the file path without extension\n(default: input): ',
+    {
+      isFile: true,
+      defaultInput: 'input',
+    }
+  )
+
+  const splitted = filePath.split('/')
+  splitted.pop()
+  const path = splitted.join('/')
+
+  console.log(`\nCSV files generated on: ${path}/output\n`)
+}
+
+main()
